7d3338db1415b6377ed2b3ff5f65eaa10a9311bf
yaswanthsaivendra <yaswanthsaivendra@gmail.com>
2021-12-03 13:58:44 +0530

    added scope script

---
+++ jsnotes/13scope.js
@@ -0,0 +1,13 @@
+/* Scope
+1. Window scope
+2. Local scope
+3. Global scope
+ */
+
+// var - function scoped
+// let - block scoped
+
+/*
+So, one should always use let and const as of best practices.
+let     - is used for any value which change.
+const   - for any constant value(and for array, object, arrow function and function expression)*/
